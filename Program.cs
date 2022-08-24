ef03bab5647d1db3f2d9231ae904d3d131f28b4c
Omer Ozekin <ozekinomer@gmail.com>
2022-08-24 10:49:01 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace eger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın!");
+
+            else if(time <= 18)
+                Console.WriteLine("İyi Günler");
+            else
+                Console.WriteLine("İyi Geceler!");
+
+        string sonuc = time<=18 ? "İyigünler!" : "İyi Geceler";
+        sonuc = time>=6 && time<11 ? "Günaydın": time<=18 ?"İyi Günler" : "İyi Geceler!";
+        Console.WriteLine(sonuc);
+
+
+
+
+        }
+    }
+}
